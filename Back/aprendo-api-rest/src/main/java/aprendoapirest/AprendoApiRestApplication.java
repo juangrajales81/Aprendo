47e863bce164e6fa6c8bee102d750d77ac9bcb6d
Juan Guillermo Grajales Villegas <juangrajales81@gmail.com>
2019-11-09 14:39:59 -0500

    Adding backend initial structure
    Initial structure created as a Spring Boot project

---
+++ Back/aprendo-api-rest/src/main/java/aprendoapirest/AprendoApiRestApplication.java
@@ -0,0 +1,13 @@
+package aprendoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendoApiRestApplication.class, args);
+	}
+
+}
